51263e2c2f8753908e364b056401a6848607e883
bihany-harsh <harshbihany7@gmail.com>
2023-03-06 16:36:07 +0530

    finally! some progress

--- manager/api/models.py
+++ manager/api/models.py
@@ -5,12 +5,11 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.core.validators import RegexValidator
-import random
-import string
 from datetime import datetime
 from django.utils import timezone
 from django.core.exceptions import ValidationError
-
+import random
+import string
 
 USER_ID_LENGTH = 8
 TXN_ID_LENGTH = 10
@@ -18,38 +17,36 @@ TXN_ID_LENGTH = 10
 
 def generate_random_string(length):
     random.seed(datetime.now().timestamp())
-    return "".join(
-        random.choice(string.ascii_uppercase + string.digits) for _ in range(length)
-    )
+    return "".join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
 
 # Create your models here.
 
-# Wallet model for storing wallet balance, created when a user is created
+# Wallet Model instantiated when a User is created
 class Wallet(models.Model):
-    user = models.OneToOneField(
-        "CustomUser", on_delete=models.CASCADE, primary_key=True
-    )
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    # list of all transactions made by the user
-    transactions = models.ManyToManyField("Transaction")
+    user = models.OneToOneField("Customuser", on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
 
     def __str__(self):
-        return f"{self.user.username}'s wallet"
-    
+        return f"{self.user.username}'s Wallet"
 
-# Transaction model for storing transaction details
+# Transaction Model instantiated when a Transaction is created
 class Transaction(models.Model):
-    # sender and receiver of the transaction
-    # throws error FOREIGN KEY constraint failed if the user is deleted
+    transaction_id = models.CharField(
+        max_length=TXN_ID_LENGTH, primary_key=True, unique=True
+    )
+    # Sender and Receiver are both Wallets
+    sender = models.ForeignKey(
+        "Wallet", on_delete=models.DO_NOTHING, related_name="sender"
+    )
+    receiver = models.ForeignKey(
+        "Wallet", on_delete=models.DO_NOTHING, related_name="receiver"
+    )
 
-    sender = models.ForeignKey("CustomUser", on_delete=models.CASCADE, related_name="sender", null=True)
-    receiver = models.ForeignKey("CustomUser", on_delete=models.CASCADE, related_name="receiver", null=True)
-
-    timestamp = models.DateTimeField(default=timezone.now)
+    timestap = models.DateTimeField(auto_now_add=True)
     transaction_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    transaction_id = models.CharField(max_length=TXN_ID_LENGTH, primary_key=True, unique=True)
 
-    # status of the transaction
+    # STATUS
     SUCCESS = 0
     FAILED = 1
     PENDING = 2
@@ -61,70 +58,63 @@ class Transaction(models.Model):
         (PENDING, "Pending"),
         (IN_REVIEW, "In Review"),
     ]
-
-    transaction_status = models.IntegerField(choices=TRANSACTION_STATUS, default=PENDING)
+    transaction_status = models.IntegerField(choices=TRANSACTION_STATUS, default=SUCCESS)
 
     class Meta:
-        unique_together = ('sender', 'receiver', 'transaction_id')
+        unique_together = ["sender", "receiver", "transaction_id"]
 
     def __str__(self):
         return f"{self.transaction_id} - {self.transaction_status}"
     
     def clean(self):
-        if self.transaction_amount < 0:
-            raise ValidationError("Transaction amount cannot be negative")
+        if self.transaction_amount <= 0:
+            raise ValidationError("Transaction amount must be greater than 0")
         if self.sender == self.receiver:
-            raise ValidationError("Sender and receiver cannot be the same")
-        if self.sender.wallet.balance < self.transaction_amount:
-            raise ValidationError("Insufficient balance")
-    
+            raise ValidationError("Sender and Receiver cannot be the same")
+        if self.sender.balance < self.transaction_amount:
+            raise ValidationError("Insufficient funds")
+
     def save(self, *args, **kwargs):
         self.clean()
-        if not self.transaction_id:
+        if self.transaction_id is None:
             self.transaction_id = generate_random_string(TXN_ID_LENGTH)
-        self.sender.wallet.balance -= self.transaction_amount
-        self.receiver.wallet.balance += self.transaction_amount
-        self.sender.wallet.save()
-        self.receiver.wallet.save()
+        self.sender.balance -= self.transaction_amount
+        self.receiver.balance += self.transaction_amount
+        self.sender.save()
+        self.receiver.save()
 
-        if self.sender.wallet.balance < 0:
-            self.transaction_status = Transaction.FAILED
+        if self.sender.balance < 0:
+            self.transaction_status = self.FAILED
+            self.sender.balance += self.transaction_amount
+            self.receiver.balance -= self.transaction_amount
+            self.sender.save()
+            self.receiver.save()
         else:
-            self.transaction_status = Transaction.SUCCESS
-        
+            self.transaction_status = self.SUCCESS
+
         super().save(*args, **kwargs)
 
-
+# Now to our User models
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, phone_number, password=None, **extra_fields):
         if not username:
-            raise ValueError("Users must have a username")
+            raise ValueError("Username is required")
         if not email:
-            raise ValueError("Users must have an email address")
+            raise ValueError("Email is required")
         if not phone_number:
-            raise ValueError("Users must have a phone number")
+            raise ValueError("Phone number is required")
 
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-            phone_number=phone_number,
-            **extra_fields
-        )
-
+        user = self.model(username=username, email=self.normalize_email(email), phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
-
-    def create_superuser(
-        self, username, email, phone_number, password=None, **extra_fields
-    ):
-        user = self.create_user(username, email, phone_number, password, **extra_fields)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-
-
+    
+    def create_superuser(self, username, email, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, phone_number, password, **extra_fields)
+    
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     class Types(models.TextChoices):
         VENDOR = "VENDOR", "vendor"
@@ -146,7 +136,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
-    user_id = models.CharField(max_length=USER_ID_LENGTH, unique=True, primary_key=True)
+    user_id = models.CharField(max_length=USER_ID_LENGTH, unique=True, primary_key=True, default=generate_random_string(USER_ID_LENGTH))
     is_vendor = models.BooleanField(default=False)
     is_customer = models.BooleanField(default=False)
 
@@ -177,33 +167,27 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         else:
             self.is_vendor = False
             self.is_customer = True
-        # instantiating a wallet for the user
+        super().save(*args, **kwargs)
+         # instantiating a wallet for the user
         if not Wallet.objects.filter(user=self).exists():
             Wallet.objects.create(user=self)
-
-        return super().save(*args, **kwargs)
 
 class VendorManager(models.Manager):
     def create_user(self, username, email, phone_number, password=None, **extra_fields):
         if not username:
-            raise ValueError("Users must have a username")
+            raise ValueError("Username is required")
         if not email:
-            raise ValueError("Users must have an email address")
+            raise ValueError("Email is required")
         if not phone_number:
-            raise ValueError("Users must have a phone number")
+            raise ValueError("Phone number is required")
 
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-            phone_number=phone_number,
-            **extra_fields
-        )
-
+        user = self.model(username=username, email=self.normalize_email(email), phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
     
-    def get_queryset(seld, *args, **kwargs):
+    def get_queryset(self, *args, **kwargs):
         return super().get_queryset(*args, **kwargs).filter(type=CustomUser.Types.VENDOR)
     
 class Vendor(CustomUser):
@@ -211,35 +195,29 @@ class Vendor(CustomUser):
 
     class Meta:
         proxy = True
-
+    
     def save(self, *args, **kwargs):
-        self.type = CustomUser.Types.VENDOR
         self.is_customer = False
         self.is_vendor = True
-
-        return super().save(*args, **kwargs)
+        self.type = self.Types.VENDOR
+        super().save(*args, **kwargs)
 
 class CustomerManager(models.Manager):
     def create_user(self, username, email, phone_number, password=None, **extra_fields):
         if not username:
-            raise ValueError("Users must have a username")
+            raise ValueError("Username is required")
         if not email:
-            raise ValueError("Users must have an email address")
+            raise ValueError("Email is required")
         if not phone_number:
-            raise ValueError("Users must have a phone number")
+            raise ValueError("Phone number is required")
 
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-            phone_number=phone_number,
-            **extra_fields
-        )
-
+        user = self.model(username=username, email=self.normalize_email(email), phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
     
-    def get_queryset(seld, *args, **kwargs):
+    def get_queryset(self, *args, **kwargs):
         return super().get_queryset(*args, **kwargs).filter(type=CustomUser.Types.CUSTOMER)
 
 class Customer(CustomUser):
@@ -247,10 +225,9 @@ class Customer(CustomUser):
 
     class Meta:
         proxy = True
-
+    
     def save(self, *args, **kwargs):
-        self.type = CustomUser.Types.CUSTOMER
         self.is_customer = True
         self.is_vendor = False
-
-        return super().save(*args, **kwargs)
+        self.type = self.Types.CUSTOMER
+        super().save(*args, **kwargs)
